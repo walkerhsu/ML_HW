ece18df9a37b02e94be619f6b393fce0ec71e6b3
walkerhsu <walkerhsu0808@gmail.com>
2023-03-22 21:12:05 +0800

    add two layers of LSTM for HW2

--- HW2/ml2023spring_hw2_1.py
+++ HW2/ml2023spring_hw2_1.py
@@ -1,3 +1,4 @@
+# %% [code] {"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-22T06:21:55.111913Z","iopub.execute_input":"2023-03-22T06:21:55.112800Z","iopub.status.idle":"2023-03-22T06:21:56.963877Z","shell.execute_reply.started":"2023-03-22T06:21:55.112698Z","shell.execute_reply":"2023-03-22T06:21:56.962708Z"}}
 # -*- coding: utf-8 -*-
 """「ML2023Spring - HW2」的副本
 
@@ -64,7 +65,7 @@ def same_seeds(seed):
 # 
 # Feel free to modify the data preprocess functions, but **do not drop any frame** (if you modify the functions, remember to check that the number of frames are the same as mentioned in the slides)
 
-# %% [code] {"id":"IJjLT8em-y9G","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-16T14:50:41.812712Z","iopub.execute_input":"2023-03-16T14:50:41.813793Z","iopub.status.idle":"2023-03-16T14:50:41.843848Z","shell.execute_reply.started":"2023-03-16T14:50:41.813748Z","shell.execute_reply":"2023-03-16T14:50:41.842434Z"}}
+# %% [code] {"id":"IJjLT8em-y9G","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-22T06:21:56.969982Z","iopub.execute_input":"2023-03-22T06:21:56.971587Z","iopub.status.idle":"2023-03-22T06:21:56.995236Z","shell.execute_reply.started":"2023-03-22T06:21:56.971548Z","shell.execute_reply":"2023-03-22T06:21:56.994049Z"}}
 import os
 import torch
 from tqdm import tqdm
@@ -161,7 +162,7 @@ def preprocess_data(split, feat_dir, phone_path, concat_nframes, train_ratio=0.8
 # %% [markdown] {"id":"us5XW_x6udZQ"}
 # # Dataset
 
-# %% [code] {"id":"Fjf5EcmJtf4e","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-16T14:50:41.850799Z","iopub.execute_input":"2023-03-16T14:50:41.853786Z","iopub.status.idle":"2023-03-16T14:50:41.865139Z","shell.execute_reply.started":"2023-03-16T14:50:41.853748Z","shell.execute_reply":"2023-03-16T14:50:41.863907Z"}}
+# %% [code] {"id":"Fjf5EcmJtf4e","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-22T06:21:56.996620Z","iopub.execute_input":"2023-03-22T06:21:56.996997Z","iopub.status.idle":"2023-03-22T06:21:57.008594Z","shell.execute_reply.started":"2023-03-22T06:21:56.996952Z","shell.execute_reply":"2023-03-22T06:21:57.007168Z"}}
 import torch
 from torch.utils.data import Dataset
 
@@ -186,7 +187,7 @@ class LibriDataset(Dataset):
 # # Model
 # Feel free to modify the structure of the model.
 
-# %% [code] {"id":"Bg-GRd7ywdrL","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-16T14:54:43.195334Z","iopub.execute_input":"2023-03-16T14:54:43.195701Z","iopub.status.idle":"2023-03-16T14:54:43.206960Z","shell.execute_reply.started":"2023-03-16T14:54:43.195670Z","shell.execute_reply":"2023-03-16T14:54:43.205882Z"}}
+# %% [code] {"id":"Bg-GRd7ywdrL","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-22T06:34:33.038899Z","iopub.execute_input":"2023-03-22T06:34:33.039280Z","iopub.status.idle":"2023-03-22T06:34:33.052828Z","shell.execute_reply.started":"2023-03-22T06:34:33.039247Z","shell.execute_reply":"2023-03-22T06:34:33.051806Z"}}
 import torch.nn as nn
 
 class BasicBlock(nn.Module):
@@ -202,6 +203,16 @@ class BasicBlock(nn.Module):
 
     def forward(self, x):
         x = self.block(x)
+        return x
+    
+class LSTM(nn.Module):
+    def __init__(self, input_dim, output_dim, hidden_layers=256, dropout=0.3, bidirectional=True):
+        super(LSTM, self).__init__()
+
+        self.LSTM = nn.LSTM(input_dim, output_dim, hidden_layers, dropout=dropout, bidirectional=bidirectional, batch_first=True)
+
+    def forward(self, x):
+        x, _ = self.LSTM(x)
         return x
 
 
@@ -219,10 +230,20 @@ class Classifier(nn.Module):
 #             nn.Linear(hidden_dim, output_dim)
 #         )
 #         RNN
-        self.rnn = nn.RNN(input_dim, hidden_dim, hidden_layers, batch_first=True)
+#         self.rnn = nn.RNN(input_dim, hidden_dim, hidden_layers, dropout=dropout, bidirectional=True, batch_first=True)
 #         LSTM
-        self.lstm = nn.LSTM(input_dim, hidden_dim, hidden_layers, dropout=dropout, bidirectional=True, batch_first=True)
-        self.fc = nn.Linear(2*hidden_dim, output_dim)
+        self.lstm = nn.Sequential(
+            LSTM(input_dim, 2*hidden_dim, hidden_layers, dropout=dropout, bidirectional=True),
+            nn.Linear(4*hidden_dim, 2*hidden_dim),
+            nn.BatchNorm1d(2*hidden_dim),
+            nn.ReLU(),
+            
+            LSTM(2*hidden_dim, hidden_dim, hidden_layers, dropout=dropout, bidirectional=True),
+            nn.Linear(2*hidden_dim, output_dim),
+            nn.BatchNorm1d(output_dim),
+            nn.ReLU(),
+        )
+        self.fc = nn.Linear(output_dim, output_dim)
     def forward(self, x):
 #         DNN
 #         x = self.fc(x)
@@ -230,7 +251,8 @@ class Classifier(nn.Module):
 #         RNN
 #         print(x.shape)
 #         print(h0.shape)
-        out, (h_m, _) = self.lstm(x)
+        out = self.lstm(x)
+#         out, _ = self.rnn(x)
         out = self.fc(out)
         
 #         print(out)
@@ -240,22 +262,22 @@ class Classifier(nn.Module):
 # %% [markdown] {"id":"TlIq8JeqvvHC"}
 # # Hyper-parameters
 
-# %% [code] {"id":"iIHn79Iav1ri","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-16T14:50:41.897473Z","iopub.execute_input":"2023-03-16T14:50:41.900395Z","iopub.status.idle":"2023-03-16T14:50:41.908938Z","shell.execute_reply.started":"2023-03-16T14:50:41.900352Z","shell.execute_reply":"2023-03-16T14:50:41.907881Z"}}
+# %% [code] {"id":"iIHn79Iav1ri","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-22T06:21:57.035288Z","iopub.execute_input":"2023-03-22T06:21:57.035790Z","iopub.status.idle":"2023-03-22T06:21:57.046626Z","shell.execute_reply.started":"2023-03-22T06:21:57.035755Z","shell.execute_reply":"2023-03-22T06:21:57.045347Z"}}
 # data prarameters
 concat_nframes = 31              # the number of frames to concat with, n must be odd (total 2k+1 = n frames)
 train_ratio = 0.8               # the ratio of data used for training, the rest will be used for validation
 
 # training parameters
 seed = 10901036                        # random seed
-batch_size = 256                # batch size
+batch_size = 512                # batch size
 num_epoch = 10                   # the number of training epoch
-learning_rate = 1e-3         # learning rate
+learning_rate = 5e-4         # learning rate
 model_path = './model.ckpt'     # the path where the checkpoint will be saved
 
 # model parameters
 input_dim = 39 * concat_nframes # the input dim of the model, you should not change the value
 hidden_layers = 2               # the number of hidden layers
-hidden_dim = 2200                # the hidden dim
+hidden_dim = 1024                # the hidden dim
 
 reload_model = False        # reload model to do further epoch training   
 dropout = 0.3              # dropout rate
@@ -263,7 +285,7 @@ dropout = 0.3              # dropout rate
 # %% [markdown] {"id":"IIUFRgG5yoDn"}
 # # Dataloader
 
-# %% [code] {"id":"c1zI3v5jyrDn","outputId":"6e7eeb1b-b76a-4846-b9b4-055d66c05661","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-16T14:50:41.915095Z","iopub.execute_input":"2023-03-16T14:50:41.918393Z","iopub.status.idle":"2023-03-16T14:51:15.058412Z","shell.execute_reply.started":"2023-03-16T14:50:41.918352Z","shell.execute_reply":"2023-03-16T14:51:15.057281Z"}}
+# %% [code] {"id":"c1zI3v5jyrDn","outputId":"6e7eeb1b-b76a-4846-b9b4-055d66c05661","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-22T06:21:57.051797Z","iopub.execute_input":"2023-03-22T06:21:57.052207Z","iopub.status.idle":"2023-03-22T06:22:27.463634Z","shell.execute_reply.started":"2023-03-22T06:21:57.052173Z","shell.execute_reply":"2023-03-22T06:22:27.462554Z"}}
 from torch.utils.data import DataLoader
 import gc
 
@@ -271,8 +293,10 @@ same_seeds(seed)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'DEVICE: {device}')
 
-feature_dir = './libriphone/feat'
-libriphone_path = './libriphone'
+# feature_dir = './libriphone/feat'
+feature_dir = "/kaggle/input/ml2023spring-hw2/libriphone/feat"
+# libriphone_path = './libriphone'
+libriphone_path = "/kaggle/input/ml2023spring-hw2/libriphone"
 
 # preprocess data
 train_X, train_y = preprocess_data(split='train', feat_dir=feature_dir, phone_path=libriphone_path, concat_nframes=concat_nframes, train_ratio=train_ratio)
@@ -293,7 +317,7 @@ val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False)
 # %% [markdown] {"id":"pwWH1KIqzxEr"}
 # # Training
 
-# %% [code] {"id":"CdMWsBs7zzNs","outputId":"426e0a6c-02bd-4f59-e45c-b05e3f28965d","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-16T14:54:46.347493Z","iopub.execute_input":"2023-03-16T14:54:46.347917Z","iopub.status.idle":"2023-03-16T14:54:53.144889Z","shell.execute_reply.started":"2023-03-16T14:54:46.347880Z","shell.execute_reply":"2023-03-16T14:54:53.143347Z"}}
+# %% [code] {"id":"CdMWsBs7zzNs","outputId":"426e0a6c-02bd-4f59-e45c-b05e3f28965d","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-22T06:34:35.857195Z","iopub.execute_input":"2023-03-22T06:34:35.857567Z","iopub.status.idle":"2023-03-22T06:34:55.889003Z","shell.execute_reply.started":"2023-03-22T06:34:35.857536Z","shell.execute_reply":"2023-03-22T06:34:55.887641Z"}}
 # create model, define a loss function, and optimizer
 model = Classifier(input_dim=input_dim, hidden_layers=hidden_layers, hidden_dim=hidden_dim, dropout=dropout).to(device)
 
@@ -361,7 +385,7 @@ for epoch in range(num_epoch):
         torch.save(model.state_dict(), model_path)
         print(f'saving model with acc {best_acc/len(val_set):.5f}')
 
-# %% [code] {"id":"ab33MxosWLmG","outputId":"0d5e2cc6-46f3-41b9-ea09-79fdf660898f","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-16T14:51:20.515154Z","iopub.status.idle":"2023-03-16T14:51:20.515921Z","shell.execute_reply.started":"2023-03-16T14:51:20.515595Z","shell.execute_reply":"2023-03-16T14:51:20.515622Z"}}
+# %% [code] {"id":"ab33MxosWLmG","outputId":"0d5e2cc6-46f3-41b9-ea09-79fdf660898f","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-22T06:22:33.907259Z","iopub.status.idle":"2023-03-22T06:22:33.907995Z","shell.execute_reply.started":"2023-03-22T06:22:33.907722Z","shell.execute_reply":"2023-03-22T06:22:33.907747Z"}}
 del train_set, val_set
 del train_loader, val_loader
 gc.collect()
@@ -370,21 +394,21 @@ gc.collect()
 # # Testing
 # Create a testing dataset, and load model from the saved checkpoint.
 
-# %% [code] {"id":"VOG1Ou0PGrhc","outputId":"3373d328-bb42-48ec-92f2-e2e935c3344c","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-16T14:51:20.517846Z","iopub.status.idle":"2023-03-16T14:51:20.518364Z","shell.execute_reply.started":"2023-03-16T14:51:20.518075Z","shell.execute_reply":"2023-03-16T14:51:20.518097Z"}}
+# %% [code] {"id":"VOG1Ou0PGrhc","outputId":"3373d328-bb42-48ec-92f2-e2e935c3344c","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-22T06:22:33.909377Z","iopub.status.idle":"2023-03-22T06:22:33.910099Z","shell.execute_reply.started":"2023-03-22T06:22:33.909829Z","shell.execute_reply":"2023-03-22T06:22:33.909854Z"}}
 # load data
 test_X = preprocess_data(split='test', feat_dir=feature_dir, phone_path=libriphone_path, concat_nframes=concat_nframes)
 test_set = LibriDataset(test_X, None)
 test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
-# %% [code] {"id":"ay0Fu8Ovkdad","outputId":"fe130106-a997-4985-fc4b-5102414afe31","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-16T14:51:20.520272Z","iopub.status.idle":"2023-03-16T14:51:20.520751Z","shell.execute_reply.started":"2023-03-16T14:51:20.520506Z","shell.execute_reply":"2023-03-16T14:51:20.520529Z"}}
+# %% [code] {"id":"ay0Fu8Ovkdad","outputId":"fe130106-a997-4985-fc4b-5102414afe31","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-22T06:22:33.911450Z","iopub.status.idle":"2023-03-22T06:22:33.912212Z","shell.execute_reply.started":"2023-03-22T06:22:33.911928Z","shell.execute_reply":"2023-03-22T06:22:33.911952Z"}}
 # load model
 model = Classifier(input_dim=input_dim, hidden_layers=hidden_layers, hidden_dim=hidden_dim).to(device)
 model.load_state_dict(torch.load(model_path))
 
 # %% [markdown] {"id":"zp-DV1p4r7Nz"}
-# Make prediction.
+# ### Make prediction.
 
-# %% [code] {"id":"84HU5GGjPqR0","outputId":"b49ffee0-1785-419d-e56c-0ddd734b2c99","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-16T14:51:20.522157Z","iopub.status.idle":"2023-03-16T14:51:20.523111Z","shell.execute_reply.started":"2023-03-16T14:51:20.522850Z","shell.execute_reply":"2023-03-16T14:51:20.522875Z"}}
+# %% [code] {"id":"84HU5GGjPqR0","outputId":"b49ffee0-1785-419d-e56c-0ddd734b2c99","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-22T06:22:33.913493Z","iopub.status.idle":"2023-03-22T06:22:33.914221Z","shell.execute_reply.started":"2023-03-22T06:22:33.913949Z","shell.execute_reply":"2023-03-22T06:22:33.913974Z"}}
 pred = np.array([], dtype=np.int32)
 
 model.eval()
@@ -399,11 +423,11 @@ with torch.no_grad():
         pred = np.concatenate((pred, test_pred.cpu().numpy()), axis=0)
 
 # %% [markdown] {"id":"wyZqy40Prz0v"}
-# Write prediction to a CSV file.
+# ### Write prediction to a CSV file.
 # 
 # After finish running this block, download the file `prediction.csv` from the files section on the left-hand side and submit it to Kaggle.
 
-# %% [code] {"id":"GuljYSPHcZir","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-16T14:51:20.524803Z","iopub.status.idle":"2023-03-16T14:51:20.525653Z","shell.execute_reply.started":"2023-03-16T14:51:20.525388Z","shell.execute_reply":"2023-03-16T14:51:20.525426Z"}}
+# %% [code] {"id":"GuljYSPHcZir","vscode":{"languageId":"python"},"jupyter":{"outputs_hidden":false},"execution":{"iopub.status.busy":"2023-03-22T06:22:33.915509Z","iopub.status.idle":"2023-03-22T06:22:33.916236Z","shell.execute_reply.started":"2023-03-22T06:22:33.915964Z","shell.execute_reply":"2023-03-22T06:22:33.915988Z"}}
 with open('prediction.csv', 'w') as f:
     f.write('Id,Class\n') 
     for i, y in enumerate(pred):
